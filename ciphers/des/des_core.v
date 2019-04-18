99655ad7b09d02b242a110bfbbfda2c85559d501
crypt-xie <xcrypt@126.com>
2019-04-18 20:12:54 +0800

    DES Cryptographic Algorithm

--- ciphers/des/des_core.v
+++ ciphers/des/des_core.v
@@ -22,7 +22,6 @@ limitations under the License.
 // Version          £º  v-1.0
 // Date				:   2019-1-25
 // Email            :   xcrypt@126.com
-// copyright        £º  XCrypt Studio
 // ------------------------------------------------------------------------------
 
 module des_core(
